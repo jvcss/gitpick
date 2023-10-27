3c346e2f9f6627121c0a2fef99113a60870b4edc
Joao Victor Cardoso dos Santos <57873270+jvcss@users.noreply.github.com>
2023-10-27 18:01:17 -0300

    feat: Update app.py
    add remove items from patch

--- app.py
+++ app.py
@@ -4,15 +4,27 @@ import hashlib
 
 st.set_page_config(layout="wide")
 
-if 'clicked_buttons' not in st.session_state: st.session_state['clicked_buttons'] = []
+if 'clicked_buttons' not in st.session_state:
+    st.session_state['clicked_buttons'] = []
 
-sha256_hash = lambda input_value: hashlib.sha256(input_value.encode('utf-8')).hexdigest()[:8]
+
+def sha256_hash(input_value): return hashlib.sha256(
+    input_value.encode('utf-8')).hexdigest()[:8]
+
 
 def clicked(col_cont):
-    if(col_cont in st.session_state['clicked_buttons']):
+    if (col_cont in st.session_state['clicked_buttons']):
         return ":green[Add Patch]"
     else:
         return "Add Patch"
+
+
+def isAdded(file_name):
+    if (file_name in st.session_state['clicked_buttons']):
+        return False
+    else:
+        return True
+
 
 with open('diffs.json', encoding="utf-8") as diff_file:
     diffs = json.load(diff_file)
@@ -21,21 +33,28 @@ with st.sidebar:
     st.title("Par A..B usando Git Diff")
     selected_branch = st.radio("---", [branches for branches in diffs.keys()])
 
-st.title(f"Difereças entre [{selected_branch.split('_')[0]}] vs [{selected_branch.split('_')[1]}]", anchor="center", help="o patch exibido é uma representação das diferenças entre a branch A e a branch B")
+st.title(f"Difereças entre [{selected_branch.split('_')[0]}] vs [{selected_branch.split('_')[1]}]",
+         anchor="center", help="o patch exibido é uma representação das diferenças entre a branch A e a branch B")
+
 for files in diffs[selected_branch]:
-    col_content, col_bring = st.columns([0.9, 0.1])
+    col_content, col_bring, col_remove = st.columns([0.8, 0.1, 0.1])
     file_hash = sha256_hash(str(diffs[selected_branch][files]))
+
     with col_content:
         with st.expander(files):
             for line in diffs[selected_branch][files]:
-                if(line.startswith('-')):
-                    st.markdown(f":red[{line}]", help="linhas que começam com --- mostram o que existe na branch (A) e não na branch (B).")
-                elif(line.startswith('+')):
-                    st.markdown(f":green[{line}]", help="linhas que começam com +++ mostram o que existe na branch (B) e não na branch (A).")
-                elif(line.startswith('@')):
-                    st.markdown(f":blue[{line}]", help="linhas que começam com @@ são cabeçalhos de contexto para mostrar onde as mudanças ocorrem no arquivo")
+                if (line.startswith('-')):
+                    st.markdown(
+                        f":red[{line}]", help="linhas que começam com --- mostram o que existe na branch (A) e não na branch (B).")
+                elif (line.startswith('+')):
+                    st.markdown(
+                        f":green[{line}]", help="linhas que começam com +++ mostram o que existe na branch (B) e não na branch (A).")
+                elif (line.startswith('@')):
+                    st.markdown(
+                        f":blue[{line}]", help="linhas que começam com @@ são cabeçalhos de contexto para mostrar onde as mudanças ocorrem no arquivo")
                 else:
                     st.markdown(f"{line}")
+
     with col_bring:
         add_patch = st.button(label=clicked(file_hash), key=f"{file_hash}")
         patch = ""
@@ -50,3 +69,24 @@ for files in diffs[selected_branch]:
                 st.rerun()
             else:
                 st.toast(f"Patch já existe {selected_branch}")
+
+    with col_remove:
+        remove_patch = st.button(
+            "Remove Patch", file_hash[::-1], disabled=isAdded(file_hash))
+
+        if (remove_patch):
+            st.toast(f"Patch removido para {selected_branch}")
+
+            with open(f"patches/{selected_branch}.patch", 'r') as file:
+                existing_patches = file.readlines()
+
+            lines_to_remove = diffs[selected_branch][files]
+
+            filtered_patches = [
+                line for line in existing_patches if line not in lines_to_remove]
+
+            with open(f"patches/{selected_branch}.patch", 'w') as file:
+                file.writelines(filtered_patches)
+
+            st.session_state['clicked_buttons'].remove(file_hash)
+            st.rerun()
